86d46356380967060a95f2c032d5486b71acb485
david akulich <davidmammedov557@gmail.com>
2026-06-19 15:07:36 +0400

    Revert "birinci commit"
    This reverts commit c5904b5695f8d9119c81f44c4b34942ae7896bd8.

--- src/routes/categories/$id.jsx
+++
@@ -1,6 +0,0 @@
-import NewsPage from '/src/components/news/category-page'
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/categories/$id')({
-  component: NewsPage,
-})
--- src/routes/index.jsx
+++
@@ -1,6 +0,0 @@
-import Main from '/src/components/main/main'
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/')({
-  component: Main,
-})
